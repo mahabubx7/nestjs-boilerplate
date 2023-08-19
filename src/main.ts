7fb8c4972b3e3382c3deb2c87709750793a9fbd6
Mahabub Alam <mahabubx7@gmail.com>
2023-08-20 01:30:25 +0600

    setup(prepared): :rocket: pre-setup-completed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || 5000);
 }
 bootstrap();
