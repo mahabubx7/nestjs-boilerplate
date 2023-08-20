63090ffdd4943fb29dddbec6cddc96c4e01832f1
Mahabub Alam <mahabubx7@gmail.com>
2023-08-20 12:42:07 +0600

    auth(modules): :hammer: added-necessary-modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/users/user.module';
+import { ArticleModule } from './modules/articles/article.module';
+import { AuthService } from './modules/auth/auth.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ArticleModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
